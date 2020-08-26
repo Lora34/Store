1e6d00c40f592ead881536e53a337b78be30ca96
Zond <zond@mail.ru>
2020-08-26 15:46:40 +0300

    'merge'

---
+++ core/base/controllers/RouteController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace core\base\controller;
+
+use core\base\settings\Settings;
+use core\base\settings\ShopSettings;
+
+class RouteControllers
+{
+    static private $_instance;
+
+    protected $routes;
+
+    protected $controller;
+    protected $inputMethod;
+    protected $outputMethod;
+    protected $parameters;
+
+    static public function getInstance(){
+        if(self::$_instance instanceof self){
+             return self::$_instance;
+        }
+
+        return self::$_instance = new self;
+    }
+
+    private function __clone() {
+
+    }
+
+    private function __construct() {
+       /*  $s = Settings::get("routes");
+        $s1 = ShopSettings::get('property1'); */
+
+        $adress_str = $_SERVER['REQUEST_URI'];
+
+        if(strrpos($adress_str, '/') === strlen($adress_str) - 1 && strrpos($adress_str, '/') !== 0) {
+            $this->redirect(rtrim($adress_str, '/'), 301);
+        }
+
+        $path = substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], 'index.php'));
+        if($path === PATH) {
+
+            $this->routes = $settings::get('routes');
+            
+            if(!this->routes) throw new RouteException('Сайт находится на техничеком обслуживании');
+
+            if(strpos($adress_str, $this->routes['admin', 'alias']) === strlen(PATH)) {
+                /*админка*/
+            }else {
+                $url = explode('/', substr($adress_str, strlen(PATH)));
+                $hrUrl = $this->routes['user']['hrUrl'];
+
+                $this->controller = $this->routes['user']['path'];
+                $route = 'user';
+            }
+            $this->createRoute($route, $url);
+
+            
+            exit();
+
+        }else{
+            try{
+                throw new \Exception('Не корректная директория сайта')
+            }
+            catch(\Exception $e){
+                exit($e->getMesagge());
+            }
+        }
+    }
+
+    private function createRoute($var, $arr){
+        $route = [];
+
+        if(!empty($arr[0])) {
+            if($this->routes[$var]['routes'][$arr[0]]){
+                $route = explode('/', $this->routes[$var]['routes'][$arr[0]]);
+
+                $this->controller .= ucfirst($route[0].'Controller');
+            }else{
+                $this->controller .= ucfirst($arr[0].'Controller');
+            }
+        }else {
+            $this->inputMethod = $route[1] ? $route[1] : $this->routes['default']['inputMethod'];
+            $this->outputMethod = $route[2] ? $route[2] : $this->routes['default']['outputMethod'];
+        }
+
+    }
+
+}
--- core/base/settings/internal_settings.php
+++ core/base/settings/internal_settings.php
@@ -1,14 +1,48 @@
 <?php
 
+defined('VG_ACCESS') or die('Access denied');
 use core\base\exceptions\RouteException;
+
+const TEMPLATE = 'templates/default';
+const ADMIN_TEMPLATE = 'core/admin/views/';
+
+const COOKIE_VERSION = '1.0.0';
+const CRYPT_KEY = '';
+const COOKIE_TIME = 60;
+const BLOCK_TIME = 3;
+
+const QTY = 8;
+const QTY_LINKS = 3;
+
+const ADMIN_CSS_JS = [
+    'styles' => [],
+    'scripts' => []
+];
+
+const USER_CSS_JS = [
+    'styles' => [],
+    'scripts' => []
+];
+
+function autoloadClasses($class_name) {
+    $class_nem = str_replace('\\', '/', $class_name);
+    if(!@include_once $class_name . '.php') {
+        throw new RouteException('Ек верное имя файла для подключения - ' .@class_name);
+    }
+}
+
+spl_autoload_register('autoloadClasses');
 
 function autoloadMainClasses($class_name){
     $class_name = str_replace('\\', '/', $class_name);
 
     if(!@include_once $class_name . '.php') {
         throw new RouteException('Не верно имя файла для подключения - ' . $class_name);
-        }
+    }
 
 }
 
 spl_autoload_register('autoloadMainClasses');
+
+
+
